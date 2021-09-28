1233759d55fc8f8104a5216d76e6816fd647314d
frank pagan <frank@cocreate.app>
2021-09-28 14:10:31 +0000

    fix: update dependencies

--- src/utils.crud.js
+++ src/utils.crud.js
@@ -1,289 +1,219 @@
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define([], function() {
-        	return factory()
-        });
-    } else if (typeof module === 'object' && module.exports) {
-        module.exports = factory();
-    } else {
-        // Browser globals (root is window)
-        root.returnExports = factory();
-  }
-}(typeof self !== 'undefined' ? self : this, function () {
-  
-  function 	__mergeObject(target, source) 
-  {
-  	target = target || {};
-  	for (let key of Object.keys(source)) {
-  		if (source[key] instanceof Object) {
-  			Object.assign(source[key], __mergeObject(target[key], source[key]))
-  		}
-  	}
-  	
-  	Object.assign(target || {}, source)
-  	return target
-  }
-  
-  function __createObject(data, path) 
-  {
-  	if (!path) return data;
-  	
-  	let keys = path.split('.')
-  	let newObject = data;
-  
-  	for (var  i = keys.length - 1; i >= 0; i--) {
-  		newObject = {[keys[i]]: newObject}				
-  	}
-  	return newObject;
-  }
-  
-  function __createArray(key, data)
-  {
-    try {
-      let item = /([\w\W]+)\[(\d+)\]/gm.exec(key)
-      if (item && item.length == 3) {
-        let arrayKey = item[1];
-        let index = parseInt(item[2]);
-        
-        if (!data[arrayKey] || !Array.isArray(data[arrayKey])) {
-          data[arrayKey] = [];
-        } 
-        data[arrayKey][index] = data[key];
-        delete data[key];
-        key = arrayKey;
-      }
-    } catch {
-      console.log('create array error');
-    }
-    return key;
-  }
-  
-  function getValueByPath(path, data)
-  {
-    try {
-      if (!path || !data) return null;
-      
-      if (data[path]) return data[path];
+(function(root, factory) {
+	if(typeof define === 'function' && define.amd) {
+		define([], function() {
+			return factory();
+		});
+	}
+	else if(typeof module === 'object' && module.exports) {
+		module.exports = factory();
+	}
+	else {
+		// Browser globals (root is window)
+		root.returnExports = factory();
+	}
+}(typeof self !== 'undefined' ? self : this, function() {
 
-      let keys = path.split('.');
-      
-      let tmp = { ...data};
-      for (var i = 0; i < keys.length; i++ ) {
-        if (!tmp) break;
-        tmp = tmp[keys[i]];
-      }
-      return tmp;
+	function __mergeObject(target, source) {
+		target = target || {};
+		for(let key of Object.keys(source)) {
+			if(source[key] instanceof Object) {
+				Object.assign(source[key], __mergeObject(target[key], source[key]));
+			}
+		}
 
-    } catch {
-      return null;
-    }
-  }
-  
-  // ToDo: currently not in use
-  function isObject(item) {
-    return (!!item) && (item.constructor === Object);
-  }
-  
-  
-  function isArray(item) {
-    return (!!item) && (item.constructor === Array);
-  }
-  
-  // ToDo: currently not in use
-  function decodeObject(data) {
-    let keys = Object.keys(data)
-    let objectData = {};
-    
-    keys.forEach((k) => {
-      k = __createArray(k, data);
-      if (k.split('.').length > 1) {
-        let newData = __createObject(data[k], k);
-        delete data[k];
-        
-        objectData = __mergeObject(objectData, newData);
-      } else {
-        objectData[k] = data[k];
-      }
-    })
-    return objectData;
-  }
+		Object.assign(target || {}, source);
+		return target;
+	}
 
-  // ToDo: currently use only by htmltags
-  function encodeObject(data) {
-    let keys = Object.keys(data);
-    let newData = {};
-    keys.forEach((k) => {
-      let data_value = data[k];
-      if (isObject(data[k])) {
-        let new_obj = encodeObject(data[k]);
-        
-        let newKeys = Object.keys(new_obj);
-        newKeys.forEach((newKey) => {
-          let value = new_obj[newKey];
-          newKey = k + "." + newKey;
-          newData[newKey] = value;
-        })
-        
-      } else if (isArray(data_value)){
-        data_value.forEach((v, index) => {
-          newData[`${k}[${index}]`] = v;
-        })
-      } else {
-        newData[k] = data[k];
-      }
-    })
-    return newData;
-  }
-  
-  function getAttr(el) {
-    if (!el) return;
-  
-    let collection = el.getAttribute('collection')
-    let document_id = el.getAttribute('document_id')
-    let name = el.getAttribute('name')
-    let isCrud = el.getAttribute('crud')
-    let isCrdt = el.getAttribute('crdt')
-    let isRealtime = el.getAttribute('realtime')
-    let isSave = el.getAttribute('save')
-    let isUpdate = el.getAttribute('udpdate')
-    let isRead = el.getAttribute('read')
-    let isListen = el.getAttribute('listen')
-    let room = el.getAttribute('room')
-    let namespace = el.getAttribute('namespace')
-    let isBroadcast = el.getAttribute('broadcast')
-    let isBroadcastSender = el.getAttribute('broadcast-sender')
-    
-    // let is_crud = isCrudAttr(el);
-    // let is_crdt = isCrdtAttr(el);
-    // let is_realtime = isRealtimeAttr(el);
-    // let is_save = isSaveAttr(el);
-    // let is_read = isReadAttr(el);
-    // let is_update = isUpdateAttr(el);
-    // let is_listen = isListenAttr(el);
-    let broadcast = isBoradcast(el)
-    let broadcast_sender = isBoradcastSender(el)
+	function __createObject(data, path) {
+		if(!path) return data;
 
-    return { 
-      collection, 
-      document_id, 
-      name, 
-      isCrud, 
-      isCrdt, 
-      isRealtime, 
-      isSave, 
-      isUpdate,
-      isRead, 
-      isListen,
-      isBroadcast,
-      isBroadcastSender,
-      room, 
-      namespace, 
-      // is_crud, 
-      // is_crdt, 
-      // is_realtime, 
-      // is_save, 
-      // is_read, 
-      // is_update, 
-      // is_listen, 
-      broadcast, 
-      broadcast_sender
-    }
-  }
-  
-  // const isCrudAttr = (el) => ( __isValueOfAttr(el, 'data-crud'));
-  // const isCrdtAttr = (el) => ( __isValueOfAttr(el, 'crdt'));
-  const isRealtimeAttr = (el) => ( __isValueOfAttr(el, 'realtime'));
-  const isReadAttr = (el) => ( __isValueOfAttr(el, 'read'));
-  const isSaveAttr = (el) => ( __isValueOfAttr(el, 'save'));
-  const isUpdateAttr = (el) => ( __isValueOfAttr(el, 'update'));
-  const isFlatAttr = (el) => ( __isValueOfAttr(el, 'data-flat'));
-  const isBoradcast = (el) => ( __isValueOfAttr(el, 'broadcast'));
-  const isBoradcastSender = (el) => ( __isValueOfAttr(el, 'broadcast-sender'));
-  const isListenAttr = (el) => ( __isValueOfAttr(el, 'listen'));
+		let keys = path.split('.');
+		let newObject = data;
 
-  function __isValueOfAttr(el, attr) {
-    if (!el) return false;
-    let flag = el.getAttribute(attr) === "false" ? false : true;
-    return flag
-  }
-  
-  // if value empty, null  or document_id="{{data.value}}" return false
+		for(var i = keys.length - 1; i >= 0; i--) {
+			newObject = {
+				[keys[i]]: newObject
+			};
+		}
+		return newObject;
+	}
 
-  function checkAttrValue(attr) {
-    if (!attr) return false;
-    if (/{{\s*([\w\W]+)\s*}}/g.test(attr)) {
-      return false;
-    }
-    
-    // ToDo temporary... Once we update crdt to not use document_id Null will no longer need
-    if (attr.toLowerCase() === "null") return false;
-    return true;
-  }
-  
+	function __createArray(key, data) {
+		try {
+			let item = /([\w\W]+)\[(\d+)\]/gm.exec(key);
+			if(item && item.length == 3) {
+				let arrayKey = item[1];
+				let index = parseInt(item[2]);
 
-  // ToDo isCrdt is not about inputs and textareas.. it is all for all elements using crdt
-  // function isCRDT(input) {
-  //   if (window && !window.CoCreate.crdt) return false;
+				if(!data[arrayKey] || !Array.isArray(data[arrayKey])) {
+					data[arrayKey] = [];
+				}
+				data[arrayKey][index] = data[key];
+				delete data[key];
+				key = arrayKey;
+			}
+		}
+		catch {
+			console.log('create array error');
+		}
+		return key;
+	}
 
-  //   const { collection, document_id, name, is_read } = getAttr(input)
-  //   if (isJsonString(collection)) return false;
-  //   if (isJsonString(name)) return false;
+	function getValueByPath(path, data) {
+		try {
+			if(!path || !data) return null;
 
-  
-  //   if ((input.tagName === "INPUT" && ["text", "email", "tel", "url"].includes(input.type)) || input.tagName === "TEXTAREA") {
-      
-  //     if (!name) return false;
-  //     if (!isRealtimeAttr(input)) return false;
-  //     if (input.getAttribute("data-unique") === "true") return false;
-  //     if (input.type === 'password') return false;
-  //     if (!isReadAttr(input)) return false;
-  //     return true;
-  //   }
-  //   return false;
-  // }
-  
-  // ToDo temporary... Once we update crdt to not use document_id Null will no longer need
-  // function checkDocumentId(document_id) {
-  //   try {
-  //     if (!document_id) return false;
-  //     if (document_id.toLowerCase() === "null") return false;
-  //     return true;
-  //   } catch(error) {
-  //     return false;
-  //   }
-  // }
-  
-  function isJsonString(str_data) {
-  try {
-    let json_data = JSON.parse(str_data);
-    if (typeof json_data === "object" && json_data != null) {
-      return true;
-    }
-    else {
-      return false;
-    }
-  }
-  catch (e) {
-    return false;
-  }
-}
-  
-  
-  return {
-    decodeObject,
-    encodeObject,
-    getAttr,
-    isRealtimeAttr,
-    isReadAttr,
-    isSaveAttr,
-    isUpdateAttr,
-    isFlatAttr,
-    checkAttrValue,
-    // isCRDT,
-    // isCrdtAttr,
-        // checkDocumentId,
-    getValueByPath,
-    isJsonString
-  }
+			if(data[path]) return data[path];
+
+			let keys = path.split('.');
+
+			let tmp = { ...data };
+			for(var i = 0; i < keys.length; i++) {
+				if(!tmp) break;
+				tmp = tmp[keys[i]];
+			}
+			return tmp;
+
+		}
+		catch {
+			return null;
+		}
+	}
+
+	// ToDo: currently not in use
+	function isObject(item) {
+		return(!!item) && (item.constructor === Object);
+	}
+
+
+	function isArray(item) {
+		return(!!item) && (item.constructor === Array);
+	}
+
+	// ToDo: currently not in use
+	function decodeObject(data) {
+		let keys = Object.keys(data);
+		let objectData = {};
+
+		keys.forEach((k) => {
+			k = __createArray(k, data);
+			if(k.split('.').length > 1) {
+				let newData = __createObject(data[k], k);
+				delete data[k];
+
+				objectData = __mergeObject(objectData, newData);
+			}
+			else {
+				objectData[k] = data[k];
+			}
+		});
+		return objectData;
+	}
+
+	// ToDo: currently use only by htmltags
+	function encodeObject(data) {
+		let keys = Object.keys(data);
+		let newData = {};
+		keys.forEach((k) => {
+			let data_value = data[k];
+			if(isObject(data[k])) {
+				let new_obj = encodeObject(data[k]);
+
+				let newKeys = Object.keys(new_obj);
+				newKeys.forEach((newKey) => {
+					let value = new_obj[newKey];
+					newKey = k + "." + newKey;
+					newData[newKey] = value;
+				});
+
+			}
+			else if(isArray(data_value)) {
+				data_value.forEach((v, index) => {
+					newData[`${k}[${index}]`] = v;
+				});
+			}
+			else {
+				newData[k] = data[k];
+			}
+		});
+		return newData;
+	}
+
+	function getAttr(el) {
+		if(!el) return;
+
+		let collection = el.getAttribute('collection');
+		let document_id = el.getAttribute('document_id');
+		let name = el.getAttribute('name');
+		let isCrud = el.getAttribute('crud');
+		let isCrdt = el.getAttribute('crdt');
+		let isRealtime = el.getAttribute('realtime');
+		let isSave = el.getAttribute('save');
+		let isUpdate = el.getAttribute('udpdate');
+		let isUpsert = el.getAttribute('upsert');
+		let isRead = el.getAttribute('read');
+		let isListen = el.getAttribute('listen');
+		let room = el.getAttribute('room');
+		let namespace = el.getAttribute('namespace');
+		let isBroadcast = el.getAttribute('broadcast');
+		let isBroadcastSender = el.getAttribute('broadcast-sender');
+
+		return {
+			collection,
+			document_id,
+			name,
+			isCrud,
+			isCrdt,
+			isRealtime,
+			isSave,
+			isUpdate,
+			isUpsert,
+			isRead,
+			isListen,
+			isBroadcast,
+			isBroadcastSender,
+			room,
+			namespace,
+		};
+	}
+
+	// if value empty, null  or document_id="{{data.value}}" return false
+	function checkAttrValue(attr) {
+		if(!attr) return false;
+		if(/{{\s*([\w\W]+)\s*}}/g.test(attr)) {
+			return false;
+		}
+
+		// ToDo temporary... Once we update crdt to not use document_id Null will no longer need
+		if(attr.toLowerCase() === "null") return false;
+		return true;
+	}
+
+
+	function isJsonString(str_data) {
+		try {
+			let json_data = JSON.parse(str_data);
+			if(typeof json_data === "object" && json_data != null) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		catch(e) {
+			return false;
+		}
+	}
+
+
+	return {
+		decodeObject,
+		encodeObject,
+		getAttr,
+		checkAttrValue,
+		getValueByPath,
+		isJsonString
+	};
 
 }));
