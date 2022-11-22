27f344de639d79157b36bad2e0ea8b250f49d194
Frank Pagan <frank@cocreate.app>
2022-11-22 12:36:14 +0000

    fix: apply src: {{source}} to CoCreate.config

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -8,11 +8,11 @@ module.exports = {
         {
             "entry": "./docs/index.html",
             "collection": "files",
-            "document_id": "6143fc56829b690010a5f330",
-            "key": "src",
             "document": {
+                "_id": "6143fc56829b690010a5f330",
                 "name": "index.html",
                 "path": "/docs/crud-client/index.html",
+                "src": "{{source}}",
                 "domains": [
                     "*",
                     "general.cocreate.app"
